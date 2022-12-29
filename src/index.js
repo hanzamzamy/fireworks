fbc119c321e216fa8b9b2e6f5f9a1c7be690ed1b
Rayhan Zamzamy <72683891+hanzamzamy@users.noreply.github.com>
2022-12-29 15:16:23 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { Fireworks, Star } from './fireworks-js/fireworks.js';
 const app = document.querySelector('#app');
 
 const fireworks = new Fireworks(app);
-for(var i = 0; i < 250; ++i){new Star(app);}
+for(var i = 0; i < 100; ++i){new Star(app);}
 
 //setInterval(function() {fireworks.launch()}, 500);
 
 app.addEventListener("mousedown", function() {fireworks.launch()});
-//app.addEventListener("touchstart", function() {fireworks.launch()});
+app.addEventListener("touchstart", function() {fireworks.launch()});
 //aa
